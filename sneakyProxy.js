217b1b3698f9bca616fa22fc7c146225fdf66c94
Rob Cameron <rwcameron@gmail.com>
2011-11-25 13:10:20 -0800

    Basic TCP socket is working

--- sneakyProxy.js
+++ sneakyProxy.js
@@ -1,12 +1,31 @@
 var net = require('net');
-var server = new.createServer(function(conn) {
+var client, serverConn, clientConn;
+
+var server = net.createServer(function(serverConn) {
+    
     console.log('server connected');
-    conn.on('end', function() {
-        console.log('server disconnected');
+    client = net.connect(22, '10.0.1.15', function(clientConn) {
+        console.log('client connected');
     });
-    conn.write('hello\r\n');
-    conn.pipe(c);
+    
+    client.on('data', function(data) {
+        serverConn.write(data);
+    });
+    
+    serverConn.on('data', function(data) {
+        console.log('recieved data');
+        client.write(data);
+    });
+    
+    //serverConn.pipe(serverConn);
+    serverConn.on('end', function() {
+        console.log('server disconnected');
+        console.log('client disconnected');
+        client.end();
+    });
+
 });
+
 server.listen(1234, function() {
     console.log('server listening');
 });
